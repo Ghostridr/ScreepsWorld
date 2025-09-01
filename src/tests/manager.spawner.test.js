b1566fe65230369bbdaa346bed274bec12ab6ca6
Ghost <92780719+Ghostridr@users.noreply.github.com>
2025-08-31 22:58:51 -0500

    feat: implement auto-spawn logic for creeps and add initial test for manager.spawner

---
+++ src/tests/manager.spawner.test.js
@@ -0,0 +1,7 @@
+// Test for manager.spawner.js
+module.exports = {
+  testManagerSpawner: function() {
+    // Add test logic for manager.spawner.js
+    console.log('manager.spawner.js test ran');
+  }
+};
