a3b8f0f323f040fe4f74fcdda23b000288701ac4
Ghost <92780719+Ghostridr@users.noreply.github.com>
2025-09-06 12:47:13 -0500

    renamed tutorials (Tutorials) to tutorial to correct GitHub (uppercase) though locally it was lowercase.

--- tutorial/tutorial-2/main.js
+++ tutorial/tutorial-2/main.js
@@ -2,14 +2,13 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
-
-    for(var name in Game.creeps) {
+    for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if (creep.memory.role === 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        if (creep.memory.role === 'upgrader') {
             roleUpgrader.run(creep);
         }
     }
-}
+};
