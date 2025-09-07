1f43428db5139b8df45da930b5995ec9a82afcb0
Ghost <92780719+Ghostridr@users.noreply.github.com>
2025-09-07 05:33:43 -0500

    refactor: remove obsolete test files for manager.spawner, role builder, role harvester, role upgrader, and utility modules

--- tests/role.builder.test.js
+++
@@ -1,7 +0,0 @@
-// Test for role.builder.js
-module.exports = {
-    testRoleBuilder: function () {
-        // Add test logic for role.builder.js
-        console.log('role.builder.js test ran');
-    },
-};
--- tests/role.upgrader.test.js
+++
@@ -1,7 +0,0 @@
-// Test for role.upgrader.js
-module.exports = {
-    testRoleUpgrader: function () {
-        // Add test logic for role.upgrader.js
-        console.log('role.upgrader.js test ran');
-    },
-};
